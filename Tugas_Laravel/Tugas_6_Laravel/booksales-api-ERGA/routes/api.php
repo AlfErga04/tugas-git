de3874177a07f71648e5303df03c86f9fa1698d8
Erga <alifian.erga04@gmail.com>
2025-05-26 20:22:58 +0700

    Push pengumpulan tugas ke-6 laravel

---
+++ Tugas_Laravel/Tugas_6_Laravel/booksales-api-ERGA/routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\AuthController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/  
+Route::apiResource('books', BookController::class)->only(['index', 'show']);
+
+Route::apiResource('authors', GenreController::class)->only(['index', 'show']);
+
+Route::apiResource('genre', GenreController::class)->only(['index', 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+
+Route::middleware(['auth:api'])->group(function() {
+
+    Route::middleware(['role:admin'])->groupt(function() {
+        Route::apiResource('books', BookController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('genre', GenreController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+    
+    });
+});
+
+
